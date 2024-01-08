3b8a35e60d89d8ca2db44c9176616f855895067a
dorobantu14 <tania.dorobantu@yahoo.com>
2024-01-08 12:37:34 +0200

    Created map screen

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -5,6 +5,7 @@ import 'package:shelter_booking/login/presentation/login_screen.dart';
 import 'package:shelter_booking/login/presentation/reset_password_screen.dart';
 import 'package:shelter_booking/login_wrapper_screen.dart';
 import 'package:shelter_booking/main_wrapper_screen.dart';
+import 'package:shelter_booking/map_screen/presentation/map_screen.dart';
 import 'package:shelter_booking/register/presentation/register_screen.dart';
 import 'package:shelter_booking/shelters_screen/home_screen/presentation/home_screen.dart';
 
@@ -25,6 +26,7 @@ part 'app_router.gr.dart';
           page: HomeScreen,
           initial: true,
         ),
+        AutoRoute(page: MapScreen),
       ],
     ),
     AutoRoute(
--- lib/shelters_screen/presentation/shelters_screen.dart
+++ lib/shelters_screen/presentation/shelters_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:core';
 
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shelter_booking/app/app_router.dart';
 import 'package:shelter_booking/core/core.dart';
 import 'package:shelter_booking/shelters_screen/domain/bloc/shelters_bloc.dart';
 import 'package:shelter_booking/shelters_screen/entity/shelter_entity.dart';
